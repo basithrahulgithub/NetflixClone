8fe5b658b221ff0e43d0600ba8feaf5ea91e1e1b
Basith Rahul <basithrahulgithub@gmail.com>
2025-01-24 11:24:41 +0530

    Add files via upload

---
+++ navigate.js
@@ -0,0 +1,30 @@
+let plus6 = document.getElementById("plus6")
+let plus5 = document.getElementById("plus5")
+let plus4 = document.getElementById("plus4")
+let plus3 = document.getElementById("plus3")
+let plus2 = document.getElementById("plus2")
+let plus1 = document.getElementById("plus1")
+
+plus1.onclick = function() {
+    document.getElementById("hide1").classList.toggle("d-none")
+}
+
+plus2.onclick = function() {
+    document.getElementById("hide2").classList.toggle("d-none")
+}
+
+plus3.onclick = function() {
+    document.getElementById("hide3").classList.toggle("d-none")
+}
+
+plus4.onclick = function() {
+    document.getElementById("hide4").classList.toggle("d-none")
+}
+
+plus5.onclick = function() {
+    document.getElementById("hide5").classList.toggle("d-none")
+}
+
+plus6.onclick = function() {
+    document.getElementById("hide6").classList.toggle("d-none")
+}
